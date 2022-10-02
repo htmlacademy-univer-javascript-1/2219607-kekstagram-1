c752a982cb8f924a0919839d0b7d326620f4d88d
Ангелина <112846779+cinnavine@users.noreply.github.com>
2022-10-03 01:21:04 +0500

    add modules

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+export const getRandom = (start, end) => {
+  if (start < 0 || end < 0) {
+    return -1;
+  }
+  if (start < end) {
+    [start, end] = [end, start];
+  }
+  const rand = start + Math.random() * (end - start);
+  return Math.round(rand);
+};
